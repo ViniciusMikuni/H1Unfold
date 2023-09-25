fdc07cc5d8f0cf408ceaf5213451d77d5f1bc7e8
ViniciusMikuni <vinicius.mikuni@gmail.com>
2023-09-24 20:06:31 -0700

    Fix cache function

--- scripts/omnifold.py
+++ scripts/omnifold.py
@@ -4,9 +4,9 @@ import tensorflow.keras
 from tensorflow.keras.layers import  Input, Dropout
 from tensorflow.keras.models import Model
 from tensorflow.keras.callbacks import EarlyStopping,ModelCheckpoint, ReduceLROnPlateau
-import os
+import os, gc
 import horovod.tensorflow.keras as hvd
-#from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split
 import logging
 from architecture import DeepSetsAtt, weighted_binary_crossentropy
 import utils
@@ -22,14 +22,19 @@ class Multifold():
                  version = 'Closure',
                  config_file='config_omnifold.json',
                  verbose=1,
+                 start = 0,
                  ):
         
         self.version = version
         self.verbose = verbose
         self.log_file =  open('log_{}.txt'.format(self.version),'w')
         self.opt = utils.LoadJson(config_file)
+        self.start = start
         self.niter = self.opt['NITER']
+        self.num_feat = self.opt['NFEAT'] 
+        self.num_event = self.opt['NEVT']
         self.nstrap=nstrap
+
         self.mc = None
         self.data=None
 
@@ -42,32 +47,46 @@ class Multifold():
     def Unfold(self):
         self.BATCH_SIZE=self.opt['BATCH_SIZE']
         self.EPOCHS=self.opt['EPOCHS']
-        self.CompileModel(float(self.opt['LR']))
+        self.CompileModel(float(self.opt['LR'])*np.sqrt(hvd.size()))
                                         
         self.weights_pull = np.ones(self.mc.weight.shape[0])
-        self.weights_push = np.ones(self.mc.weight.shape[0])
+        if self.start>0:
+            print("Loading step 2 weights from iteration {}".format(self.start))
+            model_name = '{}/Omnifold_{}_iter{}_step2.h5'.format(self.weights_folder,self.version,self.start)
+            self.model2.load_weights(model_name)
+            self.weights_push = self.reweight(self.mc.gen,self.model2,batch_size=1000)
+            #Also load model 1 to have a better starting point
+            model_name = '{}/Omnifold_{}_iter{}_step1.h5'.format(self.weights_folder,self.version,self.start)
+            self.model1.load_weights(model_name)
+        else:
+            self.weights_push = np.ones(self.mc.weight.shape[0])
             
-        for i in range(self.niter):
+        for i in range(self.start,self.niter):
             if hvd.rank()==0:print("ITERATION: {}".format(i + 1))            
             self.RunStep1(i)        
             self.RunStep2(i)            
-            self.CompileModel(float(self.opt['LR']))
+            self.CompileModel(float(self.opt['LR'])*np.sqrt(hvd.size()))
 
     def RunStep1(self,i):
         '''Data versus reco MC reweighting'''
         if hvd.rank()==0:print("RUNNING STEP 1")
+
+        if self.verbose:
+            print("Estimated total number of events: {}".format(int((self.mc.nmax + self.data.nmax))//hvd.size()))
+            print("Full number of events {}".format(np.concatenate((self.labels_mc[self.mc.pass_reco] , self.labels_data[self.data.pass_reco])).shape[0]))
+
         
         self.RunModel(
-            np.concatenate((self.mc.reco[0], self.data.reco[0])),
-            np.concatenate((self.mc.reco[1], self.data.reco[1])),
-            np.concatenate((self.labels_mc , self.labels_data )),
-            np.concatenate((self.weights_push*self.mc.pass_reco*self.mc.weight,self.data.weight*self.data.pass_reco)),
+            np.concatenate((self.labels_mc, self.labels_data)),
+            np.concatenate((self.weights_push*self.mc.weight*self.mc.pass_reco,self.data.weight*self.data.pass_reco)),
             i,self.model1,stepn=1,
             NTRAIN = (self.mc.nmax + self.data.nmax)//hvd.size(),
-            cached = i>0 #after first training cache the training data
+            cached = i>self.start #after first training cache the training data
         )
-                        
-        new_weights=self.reweight(self.mc.reco,self.model1)            
+
+
+        new_weights=self.reweight(self.tf_data1.batch(self.BATCH_SIZE),self.model1)[:self.mc.pass_reco.shape[0]]
+        
         new_weights[self.mc.pass_reco==0]=1.0 #Don't update weights where there's no reco events
         self.weights_pull = self.weights_push *new_weights
         # self.weights_pull = self.weights_pull/np.average(self.weights_pull)
@@ -77,22 +96,17 @@ class Multifold():
         if hvd.rank()==0:print("RUNNING STEP 2")
         
         self.RunModel(
-            np.concatenate((self.mc.gen[0], self.mc.gen[0])),
-            np.concatenate((self.mc.gen[1], self.mc.gen[1])),
             np.concatenate((self.labels_mc, self.labels_gen)),
             np.concatenate((self.mc.weight, self.mc.weight*self.weights_pull)),
             i,self.model2,stepn=2,
             NTRAIN = (self.mc.nmax + self.mc.nmax)//hvd.size(),
-            cached = i>0 #after first training cache the training data
+            cached = i>self.start #after first training cache the training data
         )
-
-        new_weights=self.reweight(self.mc.gen,self.model2)
+        new_weights=self.reweight(self.tf_data2.batch(self.BATCH_SIZE),self.model2)[:self.mc.pass_reco.shape[0]]
         self.weights_push = new_weights
         # self.weights_push = self.weights_push/np.average(self.weights_push)
 
     def RunModel(self,
-                 part,
-                 evt,
                  labels,
                  weights,
                  iteration,
@@ -103,10 +117,8 @@ class Multifold():
                  ):
 
         NTEST = int(0.2*NTRAIN)
-        train_data, test_data = self.cache(part,evt,labels,weights,stepn,cached,NTRAIN-NTEST)
-        
-        #train_part,test_part,train_evt,test_evt,train_labels,test_labels,train_weights,test_weights = train_test_split(part,evt,labels,weights,test_size=NTEST,shuffle=True)
-        
+        train_data, test_data = self.cache(labels,weights,stepn,cached,NTRAIN-NTEST)
+        #train_part,test_part,train_evt,test_evt,train_labels,test_labels,train_weights,test_weights = train_test_split(part,evt,labels,weights,test_size=NTEST)
         
         if self.verbose and hvd.rank()==0:
             print(80*'#')
@@ -118,8 +130,10 @@ class Multifold():
         callbacks = [
             hvd.callbacks.BroadcastGlobalVariablesCallback(0),
             hvd.callbacks.MetricAverageCallback(),
-            ReduceLROnPlateau(patience=self.opt['NPATIENCE']//2, min_lr=1e-7,verbose=verbose),
-            EarlyStopping(patience=self.opt['NPATIENCE'],restore_best_weights=True)
+            ReduceLROnPlateau(patience=self.opt['NPATIENCE']//2, min_lr=1e-5,
+                              verbose=verbose,monitor="val_loss"),
+            EarlyStopping(patience=self.opt['NPATIENCE'],restore_best_weights=True,
+                          monitor="val_loss")
         ]
         
         base_name = "Omnifold"
@@ -143,14 +157,12 @@ class Multifold():
             validation_data= test_data,
             #validation_data=([[test_part,test_evt,test_labels,test_weights]]),
             validation_steps=int(NTEST/self.BATCH_SIZE),
-            #sample_weight = train_weights,
             verbose=verbose,
             callbacks=callbacks)
-
+        del train_data, test_data
+        gc.collect()
 
     def cache(self,
-              part,
-              evt,
               label,
               weights,
               stepn,
@@ -158,40 +170,37 @@ class Multifold():
               NTRAIN
               ):
 
-        if cached:
-            if self.verbose:
-                self.log_string("Loading cached data from step {}".format(stepn))
-            if stepn ==1:
-                train_data = self.train_cached_step1
-                test_data = self.test_cached_step1
-            elif stepn==2:
-                train_data = self.train_cached_step2
-                test_data = self.test_cached_step2
-            else:
-                logging.error("ERROR: STEPN not recognized")
-        else:
+
+        if not cached:
             if self.verbose:
                 self.log_string("Creating cached data from step {}".format(stepn))
                     
             if stepn ==1:
-                tf_data = tf.data.Dataset.from_tensor_slices((
-                    {'input_1':part,
-                     'input_2':evt}, 
-                    np.stack((label,weights),axis=1))).shuffle(NTRAIN)
-                self.train_cached_step1 = tf_data.take(NTRAIN).cache().repeat().batch(self.BATCH_SIZE).prefetch(buffer_size=tf.data.AUTOTUNE)
-                self.test_cached_step1 = tf_data.skip(NTRAIN).cache().repeat().batch(self.BATCH_SIZE).prefetch(buffer_size=tf.data.AUTOTUNE)
-                train_data, test_data = self.train_cached_step1, self.test_cached_step1
+                self.tf_data1 = tf.data.Dataset.from_tensor_slices(
+                    {'input_1':np.concatenate((self.mc.reco[0], self.data.reco[0])),
+                     'input_2':np.concatenate((self.mc.reco[1], self.data.reco[1]))}).cache()
+                del self.mc.reco, self.data.reco
+                gc.collect()
             elif stepn ==2:
-                tf_data = tf.data.Dataset.from_tensor_slices((
-                    {'input_3':part,
-                     'input_4':evt}, 
-                    np.stack((label,weights),axis=1))).shuffle(NTRAIN)
-                self.train_cached_step2 = tf_data.take(NTRAIN).cache().repeat().batch(self.BATCH_SIZE).prefetch(buffer_size=tf.data.AUTOTUNE)
-                self.test_cached_step2 = tf_data.skip(NTRAIN).cache().repeat().batch(self.BATCH_SIZE).prefetch(buffer_size=tf.data.AUTOTUNE)
-                train_data, test_data = self.train_cached_step2, self.test_cached_step2
-            else:
-                logging.error("ERROR: STEPN not recognized")
+                self.tf_data2 = tf.data.Dataset.from_tensor_slices(
+                    {'input_3':np.concatenate((self.mc.gen[0], self.mc.gen[0])),
+                     'input_4':np.concatenate((self.mc.gen[1], self.mc.gen[1]))}).cache()
+                del self.mc.gen, self.data.gen
+                gc.collect()
 
+        labels = tf.data.Dataset.from_tensor_slices(np.stack((label,weights),axis=1))
+            
+        if stepn ==1:
+            data = tf.data.Dataset.zip((self.tf_data1,labels)).shuffle(label.shape[0])
+        elif stepn==2:
+            data = tf.data.Dataset.zip((self.tf_data2,labels)).shuffle(label.shape[0])
+        else:
+            logging.error("ERROR: STEPN not recognized")
+
+                
+        train_data = data.take(NTRAIN).repeat().batch(self.BATCH_SIZE).prefetch(buffer_size=tf.data.AUTOTUNE)
+        test_data = data.skip(NTRAIN).repeat().batch(self.BATCH_SIZE).prefetch(buffer_size=tf.data.AUTOTUNE)
+        
         return train_data, test_data
 
     def Preprocessing(self):
@@ -200,9 +209,21 @@ class Multifold():
 
 
     def CompileModel(self,lr):
-        opt1 = tensorflow.keras.optimizers.legacy.Adam(learning_rate=lr*np.sqrt(hvd.size()))
+        lr_schedule1 = tf.keras.experimental.CosineDecay(
+            initial_learning_rate=lr,
+            decay_steps=self.EPOCHS*int(0.8*(self.mc.nmax + self.data.nmax)/hvd.size()/self.BATCH_SIZE)
+        )
+
+        lr_schedule2 = tf.keras.experimental.CosineDecay(
+            initial_learning_rate=lr,
+            decay_steps=self.EPOCHS*int(0.8*(self.mc.nmax + self.mc.nmax)/hvd.size()/self.BATCH_SIZE)
+        )
+
+        #opt1 = tf.keras.optimizers.legacy.Adamax(learning_rate=lr_schedule1)
+        opt1 = tensorflow.keras.optimizers.legacy.Adam(learning_rate=lr)
         opt1 = hvd.DistributedOptimizer(opt1)
-        opt2 = tensorflow.keras.optimizers.legacy.Adam(learning_rate=lr*np.sqrt(hvd.size()))
+        #opt2 = tf.keras.optimizers.legacy.Adamax(learning_rate=lr_schedule2)
+        opt2 = tensorflow.keras.optimizers.legacy.Adam(learning_rate=lr)
         opt2 = hvd.DistributedOptimizer(opt2)
 
         self.model1.compile(loss=weighted_binary_crossentropy,weighted_metrics=[],
@@ -225,21 +246,19 @@ class Multifold():
 
     def PrepareModel(self):
         #Will assume same number of features for simplicity
-        num_feat = self.data.reco[0].shape[2]
-        num_event = self.data.reco[1].shape[1]
         if self.verbose:            
-            self.log_string("Preparing model architecture with: {} particle features and {} event features".format(num_feat,num_event))
+            self.log_string("Preparing model architecture with: {} particle features and {} event features".format(self.num_feat,self.num_event))
 
 
-        input_part1,input_evt1,outputs1 = DeepSetsAtt(num_feat,
-                                                      num_event,
+        input_part1,input_evt1,outputs1 = DeepSetsAtt(self.num_feat,
+                                                      self.num_event,
                                                       num_heads=self.opt['NHEADS'],
                                                       num_transformer= self.opt['NTRANSF'],
                                                       projection_dim= self.opt['NDIM'],
                                                    )
 
-        input_part2,input_evt2,outputs2 = DeepSetsAtt(num_feat,
-                                                      num_event,
+        input_part2,input_evt2,outputs2 = DeepSetsAtt(self.num_feat,
+                                                      self.num_event,
                                                       num_heads=self.opt['NHEADS'],
                                                       num_transformer= self.opt['NTRANSF'],
                                                       projection_dim= self.opt['NDIM'],
@@ -249,12 +268,16 @@ class Multifold():
         self.model1 = Model(inputs=[input_part1,input_evt1], outputs=outputs1)
         self.model2 = Model(inputs=[input_part2,input_evt2], outputs=outputs2)
         
-        if self.verbose and hvd.rank()==0:
+        if self.verbose:
             print(self.model2.summary())
 
 
-    def reweight(self,events,model):
-        f = np.nan_to_num(model.predict(events, batch_size=1000),posinf=1,neginf=0)
+    def reweight(self,events,model,batch_size=None):
+        if batch_size is None:
+           batch_size =  self.BATCH_SIZE
+        f = np.nan_to_num(model.predict(events, batch_size=batch_size
+                                        ,verbose=self.verbose)
+                          ,posinf=1,neginf=0)
         weights = f / (1. - f)
         #weights = np.clip(weights,0,10)
         weights = weights[:,0]
