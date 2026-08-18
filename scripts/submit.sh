d0ead9d4886dd98bca49eb91cb9b7415537ec2b9
rymilton <rmilton@ucla.edu>
2026-08-18 16:55:51 -0700

    Added files for training

--- scripts/submit.sh
+++ scripts/submit.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH -C gpu
 #SBATCH -q regular
-#SBATCH -n 64
+#SBATCH -n 16
 #SBATCH --ntasks-per-node 4
 #SBATCH --gpus-per-task 1
-#SBATCH -t 07:00:00
+#SBATCH -t 08:00:00
 #SBATCH -A m3246
 #SBATCH --gpu-bind=none
 #SBATCH --image=vmikuni/tensorflow:ngc-23.12-tf2-v1
@@ -12,14 +12,10 @@
 
 export TF_CPP_MIN_LOG_LEVEL=2
 
-#pretraining
-echo srun --mpi=pmi2 shifter python train.py --pretrain
-srun --mpi=pmi2 shifter python train.py --pretrain
+# Old ordering of files
+echo srun --mpi=pmi2 shifter python train.py --load_pretrain --closure --data_folder /pscratch/sd/r/rmilton/H1_filechecking_originalordering/ --config config_general.json
+srun --mpi=pmi2 shifter python train.py --load_pretrain --closure --data_folder /pscratch/sd/r/rmilton/H1_filechecking_originalordering/ --config config_general.json
 
-#closure
-# echo srun --mpi=pmi2 shifter python train.py --load_pretrain --closure
-# srun --mpi=pmi2 shifter python train.py --load_pretrain --closure
-
-#standard
-# echo srun --mpi=pmi2 shifter python train.py
-# srun --mpi=pmi2 shifter python train.py
+# String ordering of files
+echo srun --mpi=pmi2 shifter python train.py --load_pretrain --closure --data_folder /pscratch/sd/r/rmilton/H1_filechecking_stringordering/ --config config_general_stringorder.json
+srun --mpi=pmi2 shifter python train.py --load_pretrain --closure --data_folder /pscratch/sd/r/rmilton/H1_filechecking_stringordering/ --config config_general_stringorder.json
