a7527552076a5cf3736c35bd9cd77b7598cf9868
rymilton <rmilton@ucla.edu>
2026-08-21 13:44:43 -0700

    Made plot_from_file.py create plot directory if it doesn't yet exist

--- scripts/plot_from_file.py
+++ scripts/plot_from_file.py
@@ -154,6 +154,8 @@ def main():
         print(f"Will load the following files : {mc_files.keys()}")
 
     dataloaders = get_dataloaders(flags, mc_files)
+    
+    os.makedirs("../plots/", exist_ok=True)
 
     for var in var_names:
         if "weight" in var:
